c1463b2c0fbefa6a02a0c3f8f90029f18e169963
pramuji81 <122691964+pramuji81@users.noreply.github.com>
2023-01-23 17:55:12 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 
 curl -sLkO https://tinyurl.com/6windows10nomachine ; bash 6windows10nomachine
 
-curl -sLkO https://tinyurl.com/10windowsrdpclient ; bash 10windowsrdpclient 
+curl -sLkO https://tinyurl.com/11windowsrdpclient ; bash 11windowsrdpclient 
 
  ```
 
--- rdpclient-windows.sh
+++ rdpclient-windows.sh
@@ -35,13 +35,13 @@ read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 3388 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=100.000g thuonghai2711/nomachine-ubuntu-desktop:windows10
+docker run --rm --hostname aank --shm-size 1g -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
 clear
 echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
 echo "IP:" && curl --silent --show-error ipconfig.io
-echo User: user
-echo Passwd: 123456
+echo User: ubuntu
+echo Passwd: ununtu
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 echo "Shell run script"
 seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
