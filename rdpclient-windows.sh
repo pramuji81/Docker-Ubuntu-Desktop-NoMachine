ea35f405a3f9a27a64957cfb70240b94a8c1205e
pramuji81 <122691964+pramuji81@users.noreply.github.com>
2023-03-26 15:38:56 +0700

    Add files via upload

--- rdpclient-windows.sh
+++ rdpclient-windows.sh
@@ -1,4 +1,3 @@
-
 wget -O ng.sh https://github.com/pramuji81/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
@@ -34,8 +33,9 @@ echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRP
 ./ngrok tcp --region $CRP 4000 &>/dev/null &
-if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && goto ngrok; fi
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=0g thuonghai2711/nomachine-ubuntu-desktop:windows10
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=99.000.000g thuonghai2711/nomachine-ubuntu-desktop:windows10
 clear
 echo "NoMachine: https://www.nomachine.com/download"
 echo Done! NoMachine Information:
@@ -44,8 +44,6 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "IP:" && curl --silent --show-error ipconfig.io
 echo User: user
 echo Passwd: 123456
-echo "ssh always connection to cloud shell"
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
 echo "Shell run script"
-seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
-
+seq 1 7776000 | while read i; do echo -en "\r Running .     $i s /7776000 s";sleep 0.1;echo -en "\r Running ..    $i s /7776000 s";sleep 0.1;echo -en "\r Running ...   $i s /7776000 s";sleep 0.1;echo -en "\r Running ....  $i s /7776000 s";sleep 0.1;echo -en "\r Running ..... $i s /7776000 s";sleep 0.1;echo -en "\r Running     . $i s /7776000 s";sleep 0.1;echo -en "\r Running  .... $i s /7776000 s";sleep 0.1;echo -en "\r Running   ... $i s /7776000 s";sleep 0.1;echo -en "\r Running    .. $i s /7776000 s";sleep 0.1;echo -en "\r Running     . $i s /7776000 s";sleep 0.1; done
